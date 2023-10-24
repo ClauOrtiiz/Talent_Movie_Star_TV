5cc995e7639ac45c54020120b6996a9cbe107627
unknown <caroraquelq@gmail.com>
2023-10-24 15:17:22 -0500

    Añadir componentes; navegador, perfil de pelicula

---
+++ src/Componentes/Navegador/NavegadorMovil.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './navegador.css';
+
+
+export const NavegadorMovil = () => {
+  const location = useLocation();
+  const [iconActivo, setIconActivo] = useState(null);
+
+  // Función para cambiar el icono activo
+  const cambiarIconoActivo = (icono) => {
+    if (iconActivo === icono) {
+      setIconActivo(null); // Deselecciona el icono si ya está activo
+    } else {
+      setIconActivo(icono); // Selecciona el icono si no estaba activo
+    }
+  };
+
+  return (
+    <section className="section-icono-nav">
+      <Link
+        to="/MovieStar"
+        className={`nav-link ${location.pathname === '/MovieStar' || iconActivo === 'MovieStar' ? 'activo' : ''}`}
+        onMouseDown={() => cambiarIconoActivo('MovieStar')}
+      >
+        <img
+          src="../public/Iconos/home.png"
+          alt="Icono MovieStar"
+          className={`icono-nav ${iconActivo === 'MovieStar' ? 'activo' : ''}`}
+        />
+      </Link>
+      <Link
+        to="/Ingreso"
+        className={`nav-link ${location.pathname === '/Ingreso' || iconActivo === 'Ingreso' ? 'activo' : ''}`}
+        onMouseDown={() => cambiarIconoActivo('Ingreso')}
+      >
+        <img
+         src="../public/Iconos/search.png"
+          alt="Icono Ingreso"
+          className={`icono-nav ${iconActivo === 'Ingreso' ? 'activo' : ''}`}
+        />
+      </Link>
+      <Link
+        to="/Reels"
+        className={`nav-link ${location.pathname === '/Reels' || iconActivo === 'Reels' ? 'activo' : ''}`}
+        onMouseDown={() => cambiarIconoActivo('Reels')}
+      >
+        <img
+          src="../public/Iconos/playlist.png"
+          alt="Icono Reels"
+          className={`icono-nav ${iconActivo === 'Reels' ? 'activo' : ''}`}
+        />
+      </Link>
+      <Link
+        to="/Favoritos"
+        className={`nav-link ${location.pathname === '/Favoritos' || iconActivo === 'Favoritos' ? 'activo' : ''}`}
+        onMouseDown={() => cambiarIconoActivo('Favoritos')}
+      >
+        <img
+          src="../public/Iconos/hearth.png"
+          alt="Icono Favoritos"
+          className={`icono-nav ${iconActivo === 'Favoritos' ? 'activo' : ''}`}
+        />
+      </Link>
+    </section>
+  );
+};
