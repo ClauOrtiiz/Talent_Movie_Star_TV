d28c2ea672248db80301e931422a423b51c5d5fa
Claudia Ortiz LLamoca <holaclaudiia@gmail.com>
2023-10-24 15:23:01 -0500

    ADD:character cast component

---
+++ src/Componentes/DetallePelicula/Reparto/Reparto.jsx
@@ -0,0 +1,45 @@
+import TarjetaPersonaje from '../TarjetaPersonaje/tarjetaPersonaje'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { obtenerRepartoDePelicula } from '../../../services/servicesProvider'
+import './Reparto.css'
+export const ActoresReparto = () => {
+
+    const [datos, setDatos] = useState(null)
+    const idPelicula = 503616
+
+    useEffect(() => {
+        obtenerRepartoDePelicula(idPelicula)
+            .then((respuesta) => {
+                console.log(respuesta)
+                setDatos(respuesta.cast)
+            })
+    }, [])
+
+
+    return (
+        <section className="contenedorGeneral" >
+            <div>
+            <p className="titleReparto">Reparto</p>
+            </div>
+            
+            <div className="contenedor-tarjetaPersonajes" >
+            {datos ?
+                datos.map((personaje, index) =>
+                    <div  key={index} >
+                        <TarjetaPersonaje
+                            nombreActor={personaje.name}
+                            nombrePersonaje={personaje.character}
+                            imagenPersonaje={'https://image.tmdb.org/t/p/original' + personaje.profile_path}
+                        ></TarjetaPersonaje>
+                    </div>
+                )
+
+                : ""}
+                </div>
+        </section>
+    );
+};
+
+export default ActoresReparto;
