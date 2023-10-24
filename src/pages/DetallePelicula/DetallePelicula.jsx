097c962c4b241daa8ebd93be656a2fde5edf74ce
javielis <javielisrondon@gmail.com>
2023-10-24 15:20:25 -0500

    deatalles1

--- src/pages/DetallePelicula/DetallePelicula.jsx
+++ src/pages/DetallePelicula/DetallePelicula.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+// import React from 'react';
+import './Detalles.css';
+import React, { useState, useEffect } from 'react';
 
 export const DetallePelicula = () => {
+  // Clave de API de TMDb
+  const apiKey = 'https://api.themoviedb.org/3/movie/12345?api_key= 37944a78515eb243bff8628bb388d390.'; // Reemplaza con tu clave de API válida
+
+  // ID de la película que deseas obtener (reemplázalo con un ID real)
+  const movieId = '912916'; // Reemplaza '12345' con el ID de la película que deseas obtener
+
+  // URL base de la API de TMDb
+  const apiUrl = 'https://api.themoviedb.org/3/';
+
+  // Estado para almacenar la información de la película
+  const [pelicula, setPelicula] = useState(null);
+  const [favorito, setFavorito] = useState(false);
+
+  // Función para agregar/quitar la película de favoritos
+  const handleAgregarFavorito = () => {
+    setFavorito(!favorito);
+  };
+
+  // Realizar la solicitud a la API cuando el componente se monte
+  useEffect(() => {
+    // Construye la URL completa para obtener detalles de la película
+    const movieUrl = `${apiUrl}movie/${movieId}?api_key=${apiKey}`;
+
+    // Realiza la solicitud a la API
+    fetch(movieUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setPelicula(data); // Asignar los datos de la película a la variable de estado
+      })
+      .catch((error) => {
+        console.error('Error al obtener los datos de la película', error);
+      });
+  }, [apiKey, apiUrl, movieId]);
+
   return (
-    <div>DetallePelicula</div>
-  )
-}
+    <div className="detalle-pelicula">
+      {pelicula ? ( // Verificar si se han cargado los datos de la película
+        <>
+          <h1 className="titulo">{pelicula.title}</h1>
+          <h2>Fecha de Estreno: {pelicula.release_date}</h2>
+          <button onClick={handleAgregarFavorito}>
+            {favorito ? 'Quitar de favoritos' : 'Agregar a favoritos'}
+          </button>
+        </>
+      ) : (
+        <p>Cargando la información de la película...</p>
+      )}
+
+      {/* Creación de elementos de ejemplo */}
+      <button>Inicio</button>
+      <button>Categorías</button>
+    
+    </div>
+  );
+};
