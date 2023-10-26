8b3dfb3597157e092bdf6c1d58e257f0157d2d89
javielis <javielisrondon@gmail.com>
2023-10-25 19:17:17 -0500

    Detalles-Pelicula

--- src/Componentes/DetallePelicula/DescripcionPelicula/Descripcion.jsx
+++ src/Componentes/DetallePelicula/DescripcionPelicula/Descripcion.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import {buscarPeliculasPorNombre} from '../../../services/servicesProvider'
+import {obtenerDetallePelicula} from '../../../services/servicesProvider'
+import './Descripcion.css'
 export const Descripcion = () => {
 
 
-    const [datos, setDatos] = useState(null)
+    const [idPelicula, setDescripcion] = useState(null)
     let pagina = 2
     let nombrePelicula = "casa"
+    
     useEffect(() => {
-      buscarPeliculasPorNombre(nombrePelicula,pagina)
+        obtenerDetallePelicula(315162)
         .then((respuesta) => {
           console.log(respuesta)
-          setDatos(respuesta.results)
+          setDescripcion(respuesta.results)
         })
     }, [])
    
@@ -20,8 +22,8 @@ export const Descripcion = () => {
         <section className="contenedorGeneral-descripcion" >
 
             <div className="contenedor-tarjetaPersonajes" >
-                {datos ?
-                    datos.map((elemento, index) =>
+                {idPelicula?
+                idPelicula.map((elemento, index) =>
                         <div key={index} >
                             <div>
                                 <p className="tituloPelicula">{elemento.title}</p>
@@ -38,3 +40,41 @@ export const Descripcion = () => {
     );
 };
 
+
+
+// ------------
+
+
+
+// ------------------------------------------------------------------------------
+
+// import React, { useEffect, useState } from 'react';
+// import { obtenerDetallePelicula } from '../../../services/servicesProvider';
+// import './Descripcion.css';
+
+// export const Descripcion = () => {
+//     const [descripcion, setDescripcion] = useState(''); // Inicializa la descripción como una cadena vacía
+//     const idPelicula = {DetallePelicula}; // Reemplaza con el ID de la película que desees
+    
+//     console.log(idPelicula);
+
+//     useEffect(() => {
+//         obtenerDetallePelicula(278)
+//             .then((respuesta) => {
+//                 console.log(respuesta);
+//                 setDescripcion(respuesta.overview); // Asigna la descripción desde la respuesta
+//             });
+//     }, [DetallePelicula]); // Asegúrate de incluir idPelicula en la dependencia para que se actualice cuando cambie
+
+//     return (
+//         <section className="contenedorGeneral-descripcion">
+//             <div className="contenedor-tarjetaPersonajes">
+//                 <div>
+//                     <p className="tituloPelicula">Título de la película</p>
+//                 </div>
+//                 <p className='categoria'>Acción</p>
+//                 <p className='descripcion'>{descripcion}</p>
+//             </div>
+//         </section>
+//     );
+// };
--- src/pages/DetallePelicula/DetallePelicula.jsx
+++ src/pages/DetallePelicula/DetallePelicula.jsx
@@ -1,89 +1,36 @@
-import Reparto from "../../Componentes/DetallePelicula/Reparto/Reparto"
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { obtenerVideosPelicula } from '../../services/servicesProvider'
+import Reparto from "../../Componentes/DetallePelicula/Reparto/Reparto";
+import React, { useEffect, useState } from "react";
+import { obtenerVideosPelicula } from "../../services/servicesProvider";
+import "./Detalles.css";
 
-
-
- 
 export const DetallePelicula = () => {
-  const [datos, setDatos] = useState(null)
-  let idPelicula = 298618
+  const [video, setVideo] = useState(null);
+  let idPelicula = 298618;
+
   useEffect(() => {
     obtenerVideosPelicula(idPelicula)
       .then((respuesta) => {
-        console.log(respuesta)
-        setDatos(respuesta.results)
-      })
-  }, [])
+        console.log(respuesta);
+        if (respuesta.results.length > 0) {
+          setVideo(respuesta.results[0]);
+        }
+      });
+  }, []);
 
   return (
-    <div >
-      {datos ?
-        datos.map((elemento, index) =>
-          <div key={index}>
-          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${elemento.key}?si=aVicG-IXOHeD7NaU`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          </div>
-          
-        )
-
-        : ""}
-          <Reparto></Reparto>
+    <div>
+      {video && (
+        <iframe
+          width="600"
+          height="400"
+          src={`https://www.youtube.com/embed/${video.key}?si=aVicG-IXOHeD7NaU`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      )}
+      <Reparto></Reparto>
     </div>
-  )
- 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // Clave de API de TMDb
-  // const apiKey = 'https://api.themoviedb.org/3/movie/12345?api_key= 37944a78515eb243bff8628bb388d390.'; // Reemplaza con tu clave de API válida
-
-  // // ID de la película que deseas obtener (reemplázalo con un ID real)
-  // const movieId = '912916'; // Reemplaza '12345' con el ID de la película que deseas obtener
-
-  // // URL base de la API de TMDb
-  // const apiUrl = 'https://api.themoviedb.org/3/';
-
-  // // Estado para almacenar la información de la película
-  // const [pelicula, setPelicula] = useState(null);
-  // const [favorito, setFavorito] = useState(false);
-
-  // // Función para agregar/quitar la película de favoritos
-  // const handleAgregarFavorito = () => {
-  //   setFavorito(!favorito);
-  // };
-
-  // Realizar la solicitud a la API cuando el componente se monte
-  // useEffect(() => {
-  //   // Construye la URL completa para obtener detalles de la película
-  //   const movieUrl = `${apiUrl}movie/${movieId}?api_key=${apiKey}`;
-
-    // Realiza la solicitud a la API
-  //   fetch(movieUrl)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setPelicula(data); // Asignar los datos de la película a la variable de estado
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error al obtener los datos de la película', error);
-  //     });
-  // }, [apiKey, apiUrl, movieId]);
+  );
+};
