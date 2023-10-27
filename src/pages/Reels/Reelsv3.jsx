8f4113fcbae7e657cc952c8cadf2df4036fd6b5b
YesicaChui <yesicachuic@gmail.com>
2023-10-27 12:13:26 -0500

    solicitando multiples videosPelicula, juntando peliculas y agregando id, filtrando Trailer, Teaser, Clip

--- src/pages/Reels/Reelsv3.jsx
+++ src/pages/Reels/Reelsv3.jsx
@@ -1,54 +1,45 @@
+import React from 'react'
 import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import './Reels.css';
 import { obtenerVideosPelicula } from "../../services/servicesProvider";
 import { YouTubePlayer } from "./YouTubePlayer";
-export const Reels = () => {
+export const Reelsv3 = () => {
   const [videoData, setVideoData] = useState([]);
   const swiperRef = useRef(null);
   const [swiperReady, setSwiperReady] = useState(false);
   const playerRef = useRef([]);
 
   const [activeVideoIndex, setActiveVideoIndex] = useState(0);
+  const [loadingMoreVideos, setLoadingMoreVideos] = useState(false);
+  const misPeliculas = [315162, 667538, 507089,901362];
+  const [currentMovieIndex, setCurrentMovieIndex] = useState(-1);
   let videoPaused = false;
-
   useEffect(() => {
     // Llamamos a la función para cargar los videos de las dos primeras películas.
-    console.log("test")
-    const misPeliculas = [315162, 667538, 507089, 901362];
-    cargarVideos(misPeliculas);
+
+    cargarVideosPorId(misPeliculas[0], misPeliculas[1]);
   }, []);
 
-  const cargarVideos = (peliculasId) => {
-    const promises = peliculasId.map(movieId => obtenerVideosPelicula(movieId));
-
+  const cargarVideosPorId = (movieId1, movieId2) => {
     // Usamos Promise.all para realizar dos peticiones simultáneamente.
-    Promise.all(promises)
+    Promise.all([obtenerVideosPelicula(movieId1), obtenerVideosPelicula(movieId2)])
       .then((respuestas) => {
-
-        const allVideos = respuestas.flatMap(respuesta => {
-          // Obtener el id de la película actual
-          const idPelicula = respuesta.id
-          // Filtrar los videos por tipo
-          const trailers = respuesta.results.filter(video => video.type === 'Trailer');
-          const teasers = respuesta.results.filter(video => video.type === 'Teaser');
-          const clips = respuesta.results.filter(video => video.type === 'Clip');
-
-          // Obtener un máximo de 1 video de cada tipo
-          const selectedVideos = [
-            trailers.slice(0, 1),
-            teasers.slice(0, 1),
-            clips.slice(0, 1)
-          ].flat();
-          selectedVideos.forEach(video => {
-            video.idPelicula = idPelicula;
-          });
-          return selectedVideos;
-        });
-        allVideos.sort(() => Math.random() - 0.5);
-        console.log(allVideos)
-        setVideoData(allVideos);
+        const videos1 = respuestas[0].results;
+        const videos2 = respuestas[1].results;
+        
+        // Combinamos los resultados de ambas peticiones en un nuevo arreglo.
+        const newVideoData = [...videoData, ...videos1, ...videos2];
+        
+        // Actualizamos videoData una vez que ambas peticiones han terminado.
+        setVideoData(newVideoData);
+        setLoadingMoreVideos(false);
+        console.log("Cargando NUEVOS VIDEOS");
+        console.log("Videos cargados de película 1:", videos1);
+        console.log("Videos cargados de película 2:", videos2);
+        console.log("----Fin de carga----");
+        setCurrentMovieIndex(prev => prev + 2);  // Incrementamos en 2 porque hemos cargado 2 películas.
       })
       .catch((error) => {
         // Manejar errores aquí si es necesario.
@@ -56,24 +47,16 @@ export const Reels = () => {
   };
 
 
-  useEffect(() => {
-    if (swiperReady && playerRef.current[activeVideoIndex]) {
-      // Reproducir el primer video automáticamente cuando esté listo
-      playerRef.current[activeVideoIndex].playVideo();
-    }
-  }, [swiperReady, activeVideoIndex]);
-  // // Función para pausar el video actual
-  // const pauseVideo = (player) => {
-  //   player.pauseVideo();
-  // };
-
-  // // Función para reproducir el video actual
-  // const playVideo = (player) => {
-  //   player.playVideo();
-  // };
+  /*   useEffect(() => {
+      if (swiperReady && playerRef.current[activeVideoIndex]) {
+        // Reproducir el primer video automáticamente cuando esté listo
+        playerRef.current[activeVideoIndex].playVideo();
+      }
+    }, [swiperReady, activeVideoIndex]); */
 
   const handleSlideChange = () => {
     videoPaused = false;
+
     if (swiperReady && playerRef.current && videoData) {
       const activeIndex = swiperRef.current.activeIndex;
       // Pausar el video anterior (si existe)
@@ -83,9 +66,26 @@ export const Reels = () => {
 
       // Reproducir el video actual
       // playerRef.current[activeIndex].loadVideoById(videoData[activeIndex].key);
-      playerRef.current[activeIndex].playVideo();
+
+      if (playerRef.current[activeIndex]) {
+        playerRef.current[activeIndex].playVideo();
+      }
+
+
 
       setActiveVideoIndex(activeIndex);
+      if (activeIndex === videoData.length - 1 && !loadingMoreVideos) {
+
+        const nextMovieIdIndex = currentMovieIndex + 1;
+        console.log("yestest1",nextMovieIdIndex)
+        if (nextMovieIdIndex < misPeliculas.length) {
+          console.log("yestest2",nextMovieIdIndex)
+          const nextMovieId = misPeliculas[nextMovieIdIndex];
+          setLoadingMoreVideos(true);
+          console.log("yestest3",nextMovieId)
+          cargarVideosPorId(nextMovieId);
+        }
+      }
     }
   };
 
@@ -122,10 +122,8 @@ export const Reels = () => {
               <YouTubePlayer
                 videoId={elemento.key}
                 onReady={(player) => {
-                  console.log("check", player)
+                  console.log("prueba", player)
                   playerRef.current[index] = player;
-
-                  console.log("valorActual", playerRef)
                 }}
               />
             </div>
