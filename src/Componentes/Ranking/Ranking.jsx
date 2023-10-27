430345a805dd9f5bc15d99f577e0e382c4f4f29b
Claudia Ortiz LLamoca <holaclaudiia@gmail.com>
2023-10-26 23:07:59 -0500

    Actualizando: ultimos detalles

--- src/Componentes/Ranking/Ranking.jsx
+++ src/Componentes/Ranking/Ranking.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { obtenerRankeadas } from '../../services/servicesProvider'
 import { TarjetaRanking } from './TarjetaRanking';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import './Ranking.css'
 
 export const Ranking = () => {
@@ -22,22 +27,59 @@ export const Ranking = () => {
     return (
 
 
-        <section className='seccion-tarjetaRanking'>
-            
-            {dataRanking.results && dataRanking.results.slice(0, 10).map((ranking, index) => (
-                <div className='tarjetaRanking' key={index}>
-                    <TarjetaRanking
-                        numero={index + 1}
-                        idPelicula={ranking.id}
-                        tituloPelicula={ranking.original_title}
-                        posterPelicula={ranking.backdrop_path}
-                        nombre={ranking.original_title}
-                    >
-                    </TarjetaRanking>
-                </div>
-            ))}
-        </section>
+        // <section className='seccion-tarjetaRanking'>
 
+        //     {dataRanking.results && dataRanking.results.slice(0, 10).map((ranking, index) => (
+
+        //     ))}
+        // </section>
+        <Swiper
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={20}
+            slidesPerView={2}
+            navigation
+
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+            className='modificaAlturaSwiper'
+            breakpoints={{
+                768: {
+                    slidesPerView: 2,
+                },
+                800: {
+                    slidesPerView: 4,
+                },
+                900: {
+                    slidesPerView: 6,
+                },
+                1100: {
+                    slidesPerView: 4,
+                },
+            }}
+        >
+
+            {dataRanking.results && dataRanking.results.slice(0, 10).map((ranking, index) => (
+                <SwiperSlide key={ranking.id} className='swiper-slide '>
+
+
+                    <div className='tarjetaRanking' key={index}>
+                        <TarjetaRanking
+                            numero={index + 1}
+                            idPelicula={ranking.id}
+                            tituloPelicula={ranking.original_title}
+                            posterPelicula={ranking.backdrop_path}
+                            nombre={ranking.original_title}
+                        >
+                        </TarjetaRanking>
+                    </div>
+
+                </SwiperSlide>
+
+            ))}
+
+        </Swiper>
 
     );
 }
--- src/pages/ComponenteTest/Peliculas/Peliculas.jsx
+++ src/pages/ComponenteTest/Peliculas/Peliculas.jsx
@@ -32,7 +32,7 @@ export const Peliculas = () => {
   return (
 
     <body className='body'>
-      <header className='header'>
+      <header className='headerContenedor'>
         {/* <Link to={"/Reels"} className="block px-4 py-2 hover:bg-gray-100 ">Reel </Link> */}
       <Buscador/>
       </header>
