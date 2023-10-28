122ccc8fa20588956430d987ba5f8c8c7035c74b
Claudia Ortiz LLamoca <holaclaudiia@gmail.com>
2023-10-28 00:36:58 -0500

    Añadiendo: Estilos de Detalle de Pelicula

--- src/Componentes/DetallePelicula/Reparto/Reparto.jsx
+++ src/Componentes/DetallePelicula/Reparto/Reparto.jsx
@@ -3,56 +3,78 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import { obtenerRepartoDePelicula } from '../../../services/servicesProvider'
-import { obtenerVideosPelicula } from '../../../services/servicesProvider'
 import './Reparto.css'
 
-export const ActoresReparto = ({idPelicula}) => {
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-    const [datos, setDatos] = useState(null)
-    // const idPelicula = 503616
+export const ActoresReparto = ({ idPelicula }) => {
+
+    const [reparto, setReparto] = useState([])
 
     useEffect(() => {
         obtenerRepartoDePelicula(idPelicula)
             .then((respuesta) => {
                 console.log(respuesta)
-                setDatos(respuesta.cast)
-            })
-    }, [])
-
-    useEffect(() => {
-        obtenerVideosPelicula(idPelicula)
-            .then((respuesta) => {
-                console.log( 'probando videos',respuesta)
-                setDatos(respuesta.cast)
+                setReparto(respuesta.cast)
             })
     }, [])
 
 
     return (
-        <div class="contenedor-flex"> 
-        <section className="reparto" >
-            <div>
-            <p className="titulo">REPARTO</p>
-            </div>
-           
-            <div className="tarjetaPersonajes" >
-            {datos ?
-                datos.map((personaje, index) =>
-                    <div  key={index} >
-                        <TarjetaPersonaje
-                            nombreActor={personaje.name}
-                            nombrePersonaje={personaje.character}
-                            imagenPersonaje={'https://image.tmdb.org/t/p/original' + personaje.profile_path}
-                        ></TarjetaPersonaje>
-                    </div>
-                    
-                )
 
-                : ""}
-                </div>
-                
-        </section>
-        </div>
+        <article className="contenedorGeneral-reparto">
+            <div className="contenedor-reparto">
+                <p className="titulo-reparto">Reparto</p>
+                <Swiper
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={20}
+                    slidesPerView={3}
+                    navigation
+
+                    scrollbar={{ draggable: true }}
+                    onSwiper={(swiper) => console.log(swiper)}
+
+                    className='modificaAlturaSwiper'
+                    breakpoints={{
+                        768: {
+                            slidesPerView: 5,
+                        },
+                        800: {
+                            slidesPerView: 8,
+                        },
+                        900: {
+                            slidesPerView: 9,
+                        },
+                        1100: {
+                            slidesPerView: 10,
+                        },
+                    }}
+                >
+                    <section className="seccionTarjetaPersonajes-reparto" >
+                        {reparto ? reparto.map((personaje, index) =>
+                            personaje.profile_path ? (
+                                <SwiperSlide key={personaje.id} className='swiper-slide '>
+                                    <div key={index} >
+                                        <TarjetaPersonaje
+                                            nombreActor={personaje.name}
+                                            nombrePersonaje={personaje.character}
+                                            imagenPersonaje={'https://image.tmdb.org/t/p/original' + personaje.profile_path}
+                                        ></TarjetaPersonaje>
+                                    </div>
+                                </SwiperSlide>
+                            ) : null
+                        )
+
+                            : ""}
+                    </section>
+                </Swiper >
+            </div>
+        </article>
+
     );
 };
 
