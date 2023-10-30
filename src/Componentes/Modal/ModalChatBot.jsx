0b4b62aaa4faa35baf771c01dd17c09a633b14cd
unknown <caroraquelq@gmail.com>
2023-10-29 21:05:16 -0500

    Añadiendo modal-CahtBot

--- src/Componentes/Modal/ModalChatBot.jsx
+++ src/Componentes/Modal/ModalChatBot.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
+import { useEffect, useRef } from 'react';
 import Modal from 'react-modal'
 import './modalChatBot.css'
- 
+
 Modal.setAppElement('#root');
 
-Modal.defaultStyles.overlay.zIndex = 1000; 
+Modal.defaultStyles.overlay.zIndex = 1000;
 Modal.defaultStyles.content.zIndex = 1001;
 
-export const ModalChatBot = ({isOpen, closeModal }) => {
+export const ModalChatBot = ({ isOpen, closeModal }) => {
+
+
     return (
 
         <Modal
-        isOpen={isOpen}
-        onRequestClose={closeModal}
-      
-        className='seccion-modal'
-      >
-        <button onClick={closeModal} className='close-modal'>X</button>
-      </Modal>
+            isOpen={isOpen}
+            onRequestClose={closeModal}
+            className='seccion-modal'
+        >
+            <section className='seccion-input'>
+                <section className='seccion-enviar'>
+                    <h2>Hola Cinéfilo, ingrese su consulta aqui </h2>
+                      <button>Enviar</button>
+                </section>
+                <textarea className='textarea-pregunta' placeholder='Ingrese su pregunta...' />
+              
+                <p></p>
+            </section>
+
+            <button onClick={closeModal} className='close-modal'>X</button>
+        </Modal >
 
 
     )
