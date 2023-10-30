9158d8c19129aca20d65ca09966dc1956786890c
YesicaChui <yesicachuic@gmail.com>
2023-10-29 22:02:26 -0500

    comportamiento del boton cuando se espera, limpieando el teclado y poniendo una respuesta en la zona de respuesta del bot

--- src/Componentes/Modal/ModalChatBot.jsx
+++ src/Componentes/Modal/ModalChatBot.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect, useRef } from 'react';
 import Modal from 'react-modal'
 import './modalChatBot.css'
@@ -9,7 +9,50 @@ Modal.defaultStyles.overlay.zIndex = 1000;
 Modal.defaultStyles.content.zIndex = 1001;
 
 export const ModalChatBot = ({ isOpen, closeModal }) => {
+    const textareaRef = useRef(null);
+    const [respuestaChatBot, setRespuestaChatBot] = useState("")
+    const [bloquearEnvio, setBloquearEnvio] = useState(false);
+    // const [inputText, setInputText] = useState('');
+    async function llamarAchatGpt(mensage) {
+        setBloquearEnvio(true); // Bloquea el envío
+        /*
+        const misdatos = { pregunta: mensage }
+        const respuesta = await fetch('http://localhost:3100/api/miOpenAI', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(misdatos)
+        })
+        const datos = await respuesta.json()
+        return datos.data
+        */
+        setTimeout(() => {
+            console.log("respuesta chatbot")
+            setRespuestaChatBot("Respondiendo desde el chatbot")
+            setBloquearEnvio(false);
+        }, 9000)
+        setRespuestaChatBot("Un momento porfavor ....")
+    }
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' && !bloquearEnvio) {
+            e.preventDefault(); // Evita que se realice un salto de línea
+            const message = textareaRef.current.value;
+            console.log('Texto en el textarea:', message);
+            textareaRef.current.value = ""
+            llamarAchatGpt(message);
+        }
+    };
+
+    const handleEnviarClick = () => {
+        if (!bloquearEnvio) {
+            const message = textareaRef.current.value;
+            console.log('Texto en el textarea:', message);
+            textareaRef.current.value = ""
+            llamarAchatGpt(message);
+        }
+    };
 
     return (
 
@@ -21,11 +64,18 @@ export const ModalChatBot = ({ isOpen, closeModal }) => {
             <section className='seccion-input'>
                 <section className='seccion-enviar'>
                     <h2>Hola Cinéfilo, ingrese su consulta aqui </h2>
-                      <button>Enviar</button>
+                    <button
+                        style={!bloquearEnvio ? {} : {
+                            backgroundColor: 'gray',
+                            color: '#999',
+                            cursor: 'not-allowed',
+                            /* Otros estilos normales aquí */
+                        }}
+                        onClick={handleEnviarClick} disabled={bloquearEnvio}>Enviar</button>
                 </section>
-                <textarea className='textarea-pregunta' placeholder='Ingrese su pregunta...' />
-              
-                <p></p>
+                <textarea disabled={bloquearEnvio} ref={textareaRef} className='textarea-pregunta' placeholder='Ingrese su pregunta...' onKeyUp={handleKeyPress} />
+
+                <p>{respuestaChatBot}</p>
             </section>
 
             <button onClick={closeModal} className='close-modal'>X</button>
